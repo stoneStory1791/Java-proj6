a10374a0fc9f31e473490e1348e62cd78266fa67
stoneStory1791 <79501217+stoneStory1791@users.noreply.github.com>
2021-06-07 00:52:26 -0700

    Update DoubleCell.java

--- DoubleCell.java
+++ DoubleCell.java
@@ -10,61 +10,49 @@ public class DoubleCell extends HoneycombShade {
     private static final double materialPrice = 15.0;
     private static final double Rvalue = 2.7;
     private CordStyle style;
-    private boolean cordPositionOnRight = true;
+    private boolean cordPositionOnRight;
+    private double updatedHeaderPrice;
 
-    public DoubleCell(double height, double width, boolean cordPositionOnRight){
+    public DoubleCell(double height, double width, CordStyle style, boolean cordPositionOnRight){
         super (height, width, Rvalue, headerPrice, materialPrice);
         setCordStyle(style);
-        //setHeaderPrice(style);
         setCordPositionOnRight(cordPositionOnRight);
     }
 
     public void setCordStyle(CordStyle style) {
         this.style = style;
     }
-    
-    
+
     /**
      * cord position true on right, false on left
      */
     public void setCordPositionOnRight(boolean cordPositionOnRight){
         this.cordPositionOnRight = cordPositionOnRight;
     }
-    
-    
+
     @Override
     public CordStyle getCordStyle() {
         return style;
     }
 
-    // public void setHeaderPrice(CordStyle style) {
-    // switch(style) {
-    // case PULL:       headerPrice = headerPrice       ; break;
-    // case CONTINUOUS: headerPrice = headerPrice + 1.0 ; break;
-    // }
-    // }
+    @Override
+    public double getHeaderPrice() {
+        switch(style) {
+            case PULL:       updatedHeaderPrice = headerPrice       ; break;
+            case CONTINUOUS: updatedHeaderPrice = headerPrice + 2.0 ; break;
+        }
+        return updatedHeaderPrice;
+    }
 
     @Override
     public double getMaterialPrice() {
         return materialPrice;
     }
 
-    // @Override
-    // public double getHeaderPrice() {
-    // switch(style) {
-    // //case PULL:       headerPrice = headerPrice       ; break;
-    // case CONTINUOUS: continousHeaderPrice = headerPrice + 1.0 ; break;
-    // }
-    // return continousHeaderPrice;
-    // }
     public boolean getCordPositionOnRight() {
         return cordPositionOnRight;
     }
 
-    @Override
-    public double getHeaderPrice() {
-        return headerPrice;
-    }
 
     @Override
     public double getRvalue() {
@@ -74,6 +62,6 @@ public class DoubleCell extends HoneycombShade {
     @Override
     public String toString() {
         return  super.toString() + "\n" + 
-               "Cord Position on right: " + getCordPositionOnRight();
+        "Cord Position on right: " + getCordPositionOnRight();
     }
 }
